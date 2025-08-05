e2fb4a61995a898daea5f04d19be6a970a499d6f
Satyarthranjan8051 <SatyarthRanjan4488@gmail.com>
2025-08-05 20:00:11 +0530

    fix(api): use correct authToken key for cart API requests

--- src/services/cartApi.js
+++ src/services/cartApi.js
@@ -12,7 +12,7 @@ const apiClient = axios.create({
 
 // Add token to requests if user is authenticated
 apiClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem('sunlight-token');
+  const token = localStorage.getItem('authToken');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
